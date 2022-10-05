38249200f93224948a50ba8f06ae23d027c03876
Nguyen Thai Hung <19110221@student.hcmute.edu.vn>
2022-10-05 14:18:45 +0700

    ThaisHungw 14:18 api getAllProduct, getProductById

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // initial
-const express = require("express");
-require("dotenv").config();
-const app = express();
+const express = require('express')
+require('dotenv').config()
+const app = express()
 
 // middle ware for dev log
-const morgan = require("morgan");
+const morgan = require('morgan')
 
-if (process.env.NODE_ENV == "develop") {
-    app.use(morgan("dev"));
+if (process.env.NODE_ENV == 'develop') {
+  app.use(morgan('dev'))
 }
 
 //
-app.use(express.json());
+app.use(express.json())
 //routes
-const route = require("./src/routes");
+const route = require('./src/routes')
 
-route(app);
+route(app)
 
 // Error
-const errorHandler = require("./src/middleware/error");
-app.use(errorHandler);
+const errorHandler = require('./src/middleware/error')
+app.use(errorHandler)
 // db
-const db = require("./src/config/db");
-db.connect();
+const db = require('./src/config/db')
+
+db.connect()
 
 //context
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 //
-app.listen(PORT || 5000, () => console.log("Server start on port " + PORT));
+app.listen(PORT || 5000, () => console.log('Server start on port ' + PORT))
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
 // Error
-const ErrorResponse = require("../utils/ErrorResponse")
-    //Main route
-const authRoute = require("./authRoute");
-
+const ErrorResponse = require('../utils/ErrorResponse')
+//Main route
+const authRoute = require('./authRoute')
+const productRoute = require('./productRoute')
 //
 function route(app) {
-    // Auth
-    app.use("/api/auth", authRoute);
-    // main
-    app.use("/", (req, res, next) => {
-        next(new ErrorResponse(`Page not found`, 404, null, "Not found"))
-    })
+  // Auth
+  app.use('/api/auth', authRoute)
+  //product
+  app.use('/api/products', productRoute)
+  // main
+  app.use('/', (req, res, next) => {
+    next(new ErrorResponse(`Page not found`, 404, null, 'Not found'))
+  })
 }
 
-module.exports = route;
+module.exports = route
