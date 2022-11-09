100a8130368012b78de5f1c8bdeeb4b5ceeab05c
Nguyen Thai Hung <19110221@student.hcmute.edu.vn>
2022-11-09 20:27:55 +0700

    [thaishungw] api/replycomment

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -3,10 +3,14 @@ const router = express.Router()
 const verifyToken = require('../middleware/auth')
 const commentControllers = require('../controllers/commentControllers')
 
-router.route('/').post(verifyToken, commentControllers.addComment)
+router
+  .route('/:id/reply')
+  .post(verifyToken, commentControllers.addReplyComment)
+  .put(verifyToken, commentControllers.updateReplyComment)
 router
   .route('/:id')
   .put(verifyToken, commentControllers.updateComment)
   .delete(verifyToken, commentControllers.deleteComment)
+router.route('/').post(verifyToken, commentControllers.addComment)
 
 module.exports = router
