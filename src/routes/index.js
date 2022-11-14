67080e0dfbf27c65826d0e05d98b6d47f7d12b9c
Loc Le Hoang <19110237@student.hcmute.edu.vn>
2022-11-14 15:10:30 +0700

    [Loc](03:10PM): Done migrate update avatar, fix some ui bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,6 +25,11 @@ function route(app) {
 
     //product
     app.use('/api/products', productRoute)
+
+    // test
+    app.use('/test', (req, res) => {
+            res.json({ success: true, message: "Welcome nlh-ecom-system" })
+        })
         // main
     app.use('/', (req, res, next) => {
         next(new ErrorResponse(`Page not found`, 404, null, 'Not found'))
