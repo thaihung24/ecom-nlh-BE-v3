87f42cf9b3e46dbb3dcb0702bafab1c019b854b4
unknown <danbaonguyen2001@gmail.com>
2022-11-18 10:54:08 +0700

    test

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -1,66 +1,68 @@
-const catchAsyncHandler = require('../middleware/async')
-const ErrorResponse = require('../utils/ErrorResponse')
-const Item = require('../models/cart/Item')
+const catchAsyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/ErrorResponse");
+const Item = require("../models/cart/Item");
 
 class cartControllers {
   addToCart = catchAsyncHandler(async (req, res, next) => {
-    const { quantity, item } = req.body
+    const { quantity, item } = req.body;
+
     if (quantity > 0) {
       //update if item exits
       const updateItem = await Item.findOne({
         user: req.user._id,
-        'item.product': item.product,
-        'item.option': item.option,
-        'item.color': item.color,
-      })
+        "item.product": item.product,
+        "item.option": item.option,
+        "item.color": item.color,
+      });
       if (updateItem) {
-        updateItem.quantity += Number(quantity)
-        await updateItem.save()
+        updateItem.quantity += Number(quantity);
+        await updateItem.save();
       } else {
         const cart = new Item({
           user: req.user.id,
           quantity: quantity,
           item: item,
-        })
-        const createItem = await cart.save()
+        });
+        await cart.save();
       }
 
       //Get
       const Cart = await Item.find({
         user: req.user._id,
-      })
+      });
       res.status(200).json({
         success: true,
         cart: Cart,
-      })
+      });
     } else {
-      return next(new ErrorResponse('More than one', 404))
+      return next(new ErrorResponse("More than one", 404));
     }
-  })
+  });
+
   updateCart = catchAsyncHandler(async (req, res, next) => {
     // VALIDATION
-    const { itemId, quantity } = req.body
-    const cart = await Item.findById(itemId)
+    const { itemId, quantity } = req.body;
+    const cart = await Item.findById(itemId);
     if (cart && cart.user.toString() === req.user._id.toString()) {
       if (quantity > 0) {
         //update if quantity > 0
-        cart.quantity = quantity
-        await cart.save()
+        cart.quantity = quantity;
+        await cart.save();
         res.status(200).json({
           success: true,
           cart: cart,
-          message: 'Update successfully',
-        })
+          message: "Update successfully",
+        });
       } else if (quantity === 0) {
         //delete if quantity =0
-        await cart.delete()
+        await cart.delete();
         res.status(200).json({
           success: true,
-          message: 'Delete successfully',
-        })
+          message: "Delete successfully",
+        });
       }
     } else {
-      return next(new ErrorResponse('Cart not found', 404))
+      return next(new ErrorResponse("Cart not found", 404));
     }
     // // ADD TO CART
     // // Logic handler
@@ -92,21 +94,21 @@ class cartControllers {
     //   cart: newCart,
     //   message: 'Cart Added successfully',
     // })
-  })
+  });
 
   getCart = catchAsyncHandler(async (req, res, next) => {
     const cart = await Item.find({
       user: req.user._id,
-    })
+    });
 
-    if (!cart) return next(new ErrorResponse('Cart not found', 404))
+    if (!cart) return next(new ErrorResponse("Cart not found", 404));
 
     res.json({
       success: true,
       cart: cart,
-      message: 'Cart get',
-    })
-  })
+      message: "Cart get",
+    });
+  });
 
   //   deleteCart = catchAsyncHandler(async (req, res, next) => {
   //     const { productId } = req.params
@@ -123,4 +125,4 @@ class cartControllers {
   //     })
   //   })
 }
-module.exports = new cartControllers()
+module.exports = new cartControllers();
