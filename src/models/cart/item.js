de0c638920031e89a87c575b7e1256e35ce1e9d3
Loc Le Hoang <19110237@student.hcmute.edu.vn>
2022-11-18 12:55:51 +0700

    a

--- src/models/cart/item.js
+++ src/models/cart/item.js
@@ -1,42 +1,42 @@
 const mongoose = require('mongoose')
 
 const ItemSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  item: {
-    quantity: {
-      type: Number,
-      default: 1,
-      required: [true, 'Missing quantity line-item'],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
-    name: {
-      type: String,
-      required: true,
+    item: {
+        quantity: {
+            type: Number,
+            default: 1,
+            required: [true, 'Missing quantity line-item'],
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product',
+        },
+        color: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
+        option: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
     },
-    price: {
-      type: Number,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'Product',
-    },
-    color: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-    },
-    option: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-    },
-  },
 })
 
 const Item = mongoose.model('Item', ItemSchema)
