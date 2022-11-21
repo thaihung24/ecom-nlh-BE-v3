fe78249ef32c8259b1424a11f7f92215df52eae1
Loc Le Hoang <19110237@student.hcmute.edu.vn>
2022-11-21 17:13:19 +0700

    AAA

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -7,15 +7,15 @@ const verifyToken = require('../middleware/auth')
 router.route('/myorders').get(verifyToken, orderControllers.getMyOrders)
 router.route('/:id/pay').put(verifyToken, orderControllers.updateOrderToPaid)
 router
-  .route('/confirm/:id')
-  .put(verifyToken, admin, orderControllers.confirmOrder)
+    .route('/confirm/:id')
+    .put(verifyToken, admin, orderControllers.confirmOrder)
 router
-  .route('/:id')
-  .get(verifyToken, orderControllers.getOrderById)
-  .put(verifyToken, orderControllers.updateStatusOrder)
+    .route('/:id')
+    .get(verifyToken, orderControllers.getOrderById)
+    .put(verifyToken, orderControllers.updateStatusOrder)
 router
-  .route('/')
-  .post(verifyToken, orderControllers.addOrderItems)
-  .get(verifyToken, admin, orderControllers.getAllOrders)
+    .route('/')
+    .post(verifyToken, orderControllers.addOrderItems)
+    .get(verifyToken, admin, orderControllers.getAllOrders)
 
 module.exports = router
