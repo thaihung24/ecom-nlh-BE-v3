f69d228d1b5d8520cfcdcf71e800d9f9df0f467c
Loc Le Hoang <19110237@student.hcmute.edu.vn>
2022-12-06 23:32:03 +0700

    Loc

--- src/models/comment/comment.js
+++ src/models/comment/comment.js
@@ -1,61 +1,52 @@
 const mongoose = require("mongoose");
-const replySchema = mongoose.Schema(
-  {
+const replySchema = mongoose.Schema({
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
     },
     avatarUrl: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     name: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     reply: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
-  },
-  {
+}, {
     timestamps: true,
-  }
-);
-const commentSchema = mongoose.Schema(
-  {
+});
+const commentSchema = mongoose.Schema({
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
     },
     avatarUrl: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     product: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "Product",
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
     },
-    avatarUrl: {
-      type: String,
-      required: true,
-    },
+
     name: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     comment: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     replies: [replySchema],
-  },
-  {
+}, {
     timestamps: true,
-  }
-);
+});
 const Comment = mongoose.model("Comment", commentSchema);
 module.exports = Comment;
